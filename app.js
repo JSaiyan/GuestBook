3519b23bd9da928192bc83de3c853be34a702822
JSaiyan <183049245+JSaiyan@users.noreply.github.com>
2025-02-08 20:03:49 -0800

    guestbooknodejs

--- app.js
+++ app.js
@@ -7,8 +7,19 @@ const app = express();
 //Serve static files from the 'public' directory
 app.use(express.static('public'));
 
+//
+app.use(express.urlencoded({ extended: true }));
+
+
+
 //Define a port number for our server to listen on 
 const PORT = 3000;
+
+
+                // Array to store form data. memory database
+                const guestbookStorage = [];
+
+
 
 //Define a "default" route for our home page 
 app.get('/', (req,res) => 
@@ -17,16 +28,30 @@ app.get('/', (req,res) =>
         res.sendFile(`${import.meta.dirname}/views/home.html`);
     });
 
+ /*   //confirmation page route
     app.post('/', (req, res) =>
         {
             //post
             res.sendFile(`${import.meta.dirname}/views/home.html`);
         });
-
+    */
+/*
     //Define a "thank you" route
-    app.get('/thankyou', (req, res) => {
+    app.post('/thankyou', (req, res) => {
             //send our thank you page
             res.sendFile(`${import.meta.dirname}/views/thankyou.html`);
+        });
+    */
+
+     app.post('/submit', (req, res) =>{
+            console.log(req.body);
+            guestbookStorage.push(req.body);
+            res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+        });   
+
+                
+        app.get('/admin/form', (req, res) => {
+            res.send(guestbookStorage);
         });
 
     //Tell the server to listen on our specified port
